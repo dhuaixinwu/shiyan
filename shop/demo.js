b69ae9c1cab00690553b40ea7683d055cd9277a0
haoyouhao <2112343279@qq.com>
2018-10-04 20:52:17 +0800

    hi

---
+++ shop/demo.js
@@ -0,0 +1,9 @@
+//第一个商品
+var z1001="爱心屋的工作服";
+var p1001="100元"
+var t1001="http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+
+document.getElementById("z1001").innerHTML=z1001;
+document.getElementById("p1001").innerHTML=p1001;
+document.getElementById("t1001").src=t1001;
+
