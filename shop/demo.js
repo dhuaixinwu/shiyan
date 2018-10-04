6b12133a8ec84a69e7e9892067d4d17d82f64a87
haoyouhao <2112343279@qq.com>
2018-10-05 01:15:30 +0800

    hi

--- shop/demo.js
+++ shop/demo.js
@@ -1,4 +1,5 @@
-//第一个商品
+//第一行
+//1001
 {
 var t1001=
 "http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
@@ -11,7 +12,7 @@ document.getElementById("z1001").innerHTML= z1001 ;
 document.getElementById("p1001").innerHTML= p1001 ;
 document.getElementById("s1001").innerHTML= s1001 ;
 }
-//第二个商品
+//1002
 {
 var t1002=
 "http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
@@ -23,7 +24,8 @@ document.getElementById("t1002").src      = t1002 ;
 document.getElementById("z1002").innerHTML= z1002 ;
 document.getElementById("p1002").innerHTML= p1002 ;
 document.getElementById("s1002").innerHTML= s1002 ;
-}//第三个商品
+}
+//1003
 {
 var t1003=
 "http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
@@ -38,3 +40,169 @@ document.getElementById("s1003").innerHTML= s1003 ;
 }
 
 
+//第二行
+//1004
+{
+var t1004=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1004="003爱心屋的工作服";
+var p1004="200元";
+var s1004="300元";
+
+document.getElementById("t1004").src      = t1004 ;
+document.getElementById("z1004").innerHTML= z1004 ;
+document.getElementById("p1004").innerHTML= p1004 ;
+document.getElementById("s1004").innerHTML= s1004 ;
+}
+//1005
+{
+var t1005=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1005="003爱心屋的工作服";
+var p1005="200元";
+var s1005="300元";
+
+document.getElementById("t1005").src      = t1005 ;
+document.getElementById("z1005").innerHTML= z1005 ;
+document.getElementById("p1005").innerHTML= p1005 ;
+document.getElementById("s1005").innerHTML= s1005 ;
+}
+//1006
+{
+var t1006=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1006="003爱心屋的工作服";
+var p1006="200元";
+var s1006="300元";
+
+document.getElementById("t1006").src      = t1006 ;
+document.getElementById("z1006").innerHTML= z1006 ;
+document.getElementById("p1006").innerHTML= p1006 ;
+document.getElementById("s1006").innerHTML= s1006 ;
+}
+
+
+//第三行
+//1007
+{
+var t1007=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1007="003爱心屋的工作服";
+var p1007="200元";
+var s1007="300元";
+
+document.getElementById("t1007").src      = t1007 ;
+document.getElementById("z1007").innerHTML= z1007 ;
+document.getElementById("p1007").innerHTML= p1007 ;
+document.getElementById("s1007").innerHTML= s1007 ;
+}
+//1008
+{
+var t1008=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1008="003爱心屋的工作服";
+var p1008="200元";
+var s1008="300元";
+
+document.getElementById("t1008").src      = t1008 ;
+document.getElementById("z1008").innerHTML= z1008 ;
+document.getElementById("p1008").innerHTML= p1008 ;
+document.getElementById("s1008").innerHTML= s1008 ;
+}
+//1009
+{
+var t1009=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1009="003爱心屋的工作服";
+var p1009="200元";
+var s1009="300元";
+
+document.getElementById("t1009").src      = t1009 ;
+document.getElementById("z1009").innerHTML= z1009 ;
+document.getElementById("p1009").innerHTML= p1009 ;
+document.getElementById("s1009").innerHTML= s1009 ;
+}
+
+
+//第四行
+//1010
+{
+var t1010=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1010="003爱心屋的工作服";
+var p1010="200元";
+var s1010="300元";
+
+document.getElementById("t1010").src      = t1010 ;
+document.getElementById("z1010").innerHTML= z1010 ;
+document.getElementById("p1010").innerHTML= p1010 ;
+document.getElementById("s1010").innerHTML= s1010 ;
+}
+//1008
+{
+var t1008=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1008="003爱心屋的工作服";
+var p1008="200元";
+var s1008="300元";
+
+document.getElementById("t1008").src      = t1008 ;
+document.getElementById("z1008").innerHTML= z1008 ;
+document.getElementById("p1008").innerHTML= p1008 ;
+document.getElementById("s1008").innerHTML= s1008 ;
+}
+//1009
+{
+var t1009=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1009="003爱心屋的工作服";
+var p1009="200元";
+var s1009="300元";
+
+document.getElementById("t1009").src      = t1009 ;
+document.getElementById("z1009").innerHTML= z1009 ;
+document.getElementById("p1009").innerHTML= p1009 ;
+document.getElementById("s1009").innerHTML= s1009 ;
+}
+
+
+//第五行
+//1007
+{
+var t1007=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1007="003爱心屋的工作服";
+var p1007="200元";
+var s1007="300元";
+
+document.getElementById("t1007").src      = t1007 ;
+document.getElementById("z1007").innerHTML= z1007 ;
+document.getElementById("p1007").innerHTML= p1007 ;
+document.getElementById("s1007").innerHTML= s1007 ;
+}
+//1008
+{
+var t1008=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1008="003爱心屋的工作服";
+var p1008="200元";
+var s1008="300元";
+
+document.getElementById("t1008").src      = t1008 ;
+document.getElementById("z1008").innerHTML= z1008 ;
+document.getElementById("p1008").innerHTML= p1008 ;
+document.getElementById("s1008").innerHTML= s1008 ;
+}
+//1009
+{
+var t1009=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1009="003爱心屋的工作服";
+var p1009="200元";
+var s1009="300元";
+
+document.getElementById("t1009").src      = t1009 ;
+document.getElementById("z1009").innerHTML= z1009 ;
+document.getElementById("p1009").innerHTML= p1009 ;
+document.getElementById("s1009").innerHTML= s1009 ;
+}
