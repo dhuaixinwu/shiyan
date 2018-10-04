4520c1d380b3c633c03fc497c39f7d683b211bee
haoyouhao <2112343279@qq.com>
2018-10-05 00:56:04 +0800

    hi

--- shop/demo.js
+++ shop/demo.js
@@ -1,9 +1,40 @@
 //第一个商品
-var z1001="爱心屋的工作服";
-var p1001="100元";
-var t1001="http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+{
+var t1001=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1001="001爱心屋的工作服";
+var p1001="200元";
+var s1001="300元";
 
-document.getElementById("z1001").innerHTML=z1001;
-document.getElementById("p1001").innerHTML=p1001;
-document.getElementById("t1001").src=t1001;
+document.getElementById("t1001").src      = t1001 ;
+document.getElementById("z1001").innerHTML= z1001 ;
+document.getElementById("p1001").innerHTML= p1001 ;
+document.getElementById("s1001").innerHTML= s1001 ;
+}
+//第二个商品
+{
+var t1002=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1002="002爱心屋的工作服";
+var p1002="200元";
+var s1002="300元";
+
+document.getElementById("t1002").src      = t1002 ;
+document.getElementById("z1002").innerHTML= z1002 ;
+document.getElementById("p1002").innerHTML= p1002 ;
+document.getElementById("s1002").innerHTML= s1002 ;
+}//第三个商品
+{
+var t1003=
+"http://bmob-cdn-20135.b0.upaiyun.com/2018/10/03/c550c90b40592cfb806e13c72680af54.jpg";
+var z1003="003爱心屋的工作服";
+var p1003="200元";
+var s1003="300元";
+
+document.getElementById("t1003").src      = t1003 ;
+document.getElementById("z1003").innerHTML= z1003 ;
+document.getElementById("p1003").innerHTML= p1003 ;
+document.getElementById("s1003").innerHTML= s1003 ;
+}
+
 
