f388edc352d8e6ea75beff16e9ff53a3851b528b
haoyouhao <2112343279@qq.com>
2018-10-05 16:51:20 +0800

    hi

--- shop/学习用品/笔/demo.js
+++ shop/学习用品/笔/demo.js
@@ -1,8 +1,9 @@
+
 //第一行
 //1001
 {
 document.getElementById("z1001").innerHTML= "001爱心屋的工作服";
-document.getElementById("p1001").innerHTML= "200元";
+document.getElementById("p1001").innerHTML= "100元";
 document.getElementById("t1001").src= 
 "      http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg               ";
 }
@@ -15,7 +16,7 @@ document.getElementById("t1002").src=
 }
 //1003
 {
-document.getElementById("z1003").innerHTML= "001爱心屋的工作服";
+document.getElementById("z1003").innerHTML= "001深夜福利";
 document.getElementById("p1003").innerHTML= "200元";
 document.getElementById("t1003").src= 
 "      http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif                ";
