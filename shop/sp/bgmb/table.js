e1c70f161272e739ff9881bd801c26258e3be51d
haoyouhao <2112343279@qq.com>
2018-10-12 00:06:28 +0800

    hi

--- shop/sp/bgmb/table.js
+++ shop/sp/bgmb/table.js
@@ -1,12 +1,12 @@
 document.body.onload = table(); function table() { var div1 = document.getElementById("tb1"); var code = ''; code.id="tab"; 
 
-code+="<tr><td>1</td><td>献爱心</td><td>无价</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/11/ec869a76404f428980faa8d32a75b463.gif</td><td>献爱心是一种美德无价</td><td>献爱心</td><td>献爱心</td><td>献爱心</td></tr>";
+code+="<tr><td>1</td><td>献爱心</td><td>无价</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/11/ec869a76404f428980faa8d32a75b463.gif</td><td>献爱心是一种美德无价</td><td>0</td><td>献爱心</td><td>献爱心</td></tr>";
 
 code+="<tr><td>2</td><td>名称2</td><td>101元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>空</td><td>b</td><td>c</td><td>d</td></tr>";
 
 code+="<tr><td>3</td><td>名称3</td><td>102元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>空</td><td>b</td><td>c</td><td>d</td></tr>";
 
-code+="<tr><td>4</td><td>名称4</td><td>103元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>空</td><td> </td><td> </td><td> </td></tr>";
+code+="<tr><td>4</td><td>名称4</td><td>103元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>空</td><td>0</td><td> </td><td> </td></tr>";
 
 code+="<tr><td>5</td><td>名称5</td><td>104元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>空</td><td> </td><td> </td><td> </td></tr>";
 
