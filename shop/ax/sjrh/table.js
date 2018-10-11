51b69824ba069c21611d9f1818f9ed2eed4e9608
haoyouhao <2112343279@qq.com>
2018-10-11 21:27:16 +0800

    hi

---
+++ shop/ax/sjrh/table.js
@@ -0,0 +1,22 @@
+document.body.onload = table(); function table() { var div1 = document.getElementById("tb1"); var code = ''; code.id="tab"; 
+
+code+="<tr><td>1</td><td>名称1</td><td>100元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td></td><td></td><td></td></tr>";
+
+code+="<tr><td>2</td><td>名称2</td><td>101元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+
+code+="<tr><td>3</td><td>名称3</td><td>102元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+
+code+="<tr><td>4</td><td>名称4</td><td>103元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+
+code+="<tr><td>5</td><td>名称5</td><td>104元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+
+code+="<tr><td>6</td><td>名称6</td><td>105元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+
+code+="<tr><td>7</td><td>名称7</td><td>106元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+
+code+="<tr><td>8</td><td>名称8</td><td>107元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+
+code+="<tr><td>9</td><td>名称9</td><td>108元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+
+code+="<tr><td>10</td><td>名称10</td><td>109元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+div1.innerHTML=code;} 
