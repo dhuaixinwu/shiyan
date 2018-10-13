bfe2b604c3712613c104d5bd5b13e62b2770727f
haoyouhao <2112343279@qq.com>
2018-10-13 18:15:57 +0800

    ih

--- shop/ax/sjrh/table.js
+++ shop/ax/sjrh/table.js
@@ -1,22 +1,18 @@
-document.body.onload = table(); function table() { var div1 = document.getElementById("tb1"); var code = ''; code.id="tab"; 
+﻿document.body.onload = table(); function table() { var div1 = document.getElementById("tb1"); var code = ''; code.id="tab"; 
 
-code+="<tr><td>1</td><td>名称1</td><td>100元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td></td><td></td><td></td></tr>";
+code+="<tr><td>1</td><td>献爱心</td><td>无价</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/11/ec869a76404f428980faa8d32a75b463.gif</td><td>献爱心是一种美德无价</td><td>0</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>2</td><td>名称2</td><td>101元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+code+="<tr><td>2</td><td>belle jargin面膜</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/6ada656d4078e77880a653c4a0215063.jpg</td><td>belle jargin面膜</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/726f956a4030f91380b03a5b04abe4e3.png</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>3</td><td>名称3</td><td>102元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+code+="<tr><td>3</td><td>韩束面膜</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/ada4771d4002381180045e26c30b54d3.jpg</td><td>韩束面膜</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/7d062eb4400bfa7480770d57507d6c30.png</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>4</td><td>名称4</td><td>103元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+code+="<tr><td>4</td><td>韩束祛黑头三部曲</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/082f136d401892cb8076ec43c2fbda36.jpg</td><td>韩束祛黑头三部曲</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/90e2b203403a217d80e82ac733b3e5ce.png</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>5</td><td>名称5</td><td>104元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+code+="<tr><td>5</td><td>时克牙膏</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/01d9bb65401ea1d8805be3fbe8e9657f.jpg</td><td>时克牙膏</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/bf28d72540e5e67c802391da84483c84.jpg</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>6</td><td>名称6</td><td>105元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+code+="<tr><td>6</td><td>吾尊精华水</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/c8011d074013a6a2805741546e821201.jpg</td><td>吾尊精华水</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/2cca7c1f40075af38095f5c6ab5e359b.jpg</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>7</td><td>名称7</td><td>106元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+code+="<tr><td>7</td><td>一叶子香氛 </td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/f7ba28c1407d98cc80bd24940e20c11e.jpg</td><td>一叶子香氛 </td><td>0</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>8</td><td>名称8</td><td>107元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
-
-code+="<tr><td>9</td><td>名称9</td><td>108元</td><td>http://pic5.nipic.com/20100221/2839526_090902782678_2.jpg</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
-
-code+="<tr><td>10</td><td>名称10</td><td>109元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/05/9cad7d2f402a5b2d80d802e799e1017f.gif</td><td>暗室逢灯</td><td>b</td><td>c</td><td>d</td></tr>";
+code+="<tr><td>8</td><td>献爱心</td><td>无价</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/11/ec869a76404f428980faa8d32a75b463.gif</td><td>献爱心是一种美德无价</td><td>0</td><td>0</td><td>0</td></tr>";
 div1.innerHTML=code;} 
