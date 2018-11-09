3ffc2937f97aefd36e798c236c4e224bd610f7ce
haoyouhao <2112343279@qq.com>
2018-11-09 14:37:12 +0800

    hi

--- shop/ax/sjrh/table.js
+++ shop/ax/sjrh/table.js
@@ -1,18 +1,34 @@
 ﻿document.body.onload = table(); function table() { var div1 = document.getElementById("tb1"); var code = ''; code.id="tab"; 
 
-code+="<tr><td>1</td><td>献爱心</td><td>无价</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/11/ec869a76404f428980faa8d32a75b463.gif</td><td>献爱心是一种美德无价</td><td>0</td><td>0</td><td>0</td></tr>";
+code+="<tr><td>1</td><td>BRTC茉莉保湿BB霜35g</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/814702ca4081312c809cc61c1e65b93c.jpg</td><td>上海基金会捐赠。保质期到2019年5月29号</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/0dfd8999409a01ec80ca8d7e021a7556.jpg</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>2</td><td>belle jargin面膜</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/6ada656d4078e77880a653c4a0215063.jpg</td><td>belle jargin面膜</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/726f956a4030f91380b03a5b04abe4e3.png</td><td>0</td><td>0</td></tr>";
+code+="<tr><td>2</td><td>BRTC茉莉花保湿粉饼13g</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/75eef6a34053366e8047025576aaca98.jpg</td><td>上海基金会捐赠。保质期到2019年5月29号</td><td>0</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>3</td><td>韩束面膜</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/ada4771d4002381180045e26c30b54d3.jpg</td><td>韩束面膜</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/7d062eb4400bfa7480770d57507d6c30.png</td><td>0</td><td>0</td></tr>";
+code+="<tr><td>3</td><td>BRTC嫩肌护肤精华水150ml</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/7ded951f40b94f4480a6b99ea105ff2a.jpg</td><td>上海基金会捐赠。保质期到2019年5月31号</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/e0b06833406e247d80256535150376fd.jpg</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>4</td><td>韩束祛黑头三部曲</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/082f136d401892cb8076ec43c2fbda36.jpg</td><td>韩束祛黑头三部曲</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/90e2b203403a217d80e82ac733b3e5ce.png</td><td>0</td><td>0</td></tr>";
+code+="<tr><td>4</td><td>BRTC舒缓保湿BB卸妆乳200ml</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/157ad5174099fd02808fd6318d02073f.jpg</td><td>上海基金会捐赠。保质期到2019年6月16号</td><td>0</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>5</td><td>时克牙膏</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/01d9bb65401ea1d8805be3fbe8e9657f.jpg</td><td>时克牙膏</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/bf28d72540e5e67c802391da84483c84.jpg</td><td>0</td><td>0</td></tr>";
+code+="<tr><td>5</td><td>蜂胶保湿洁面乳100ml</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/e85a2dd440d016fe80214c83d078d6ce.jpg</td><td>上海基金会捐赠。保质期到2019年7月17号</td><td>0</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>6</td><td>吾尊精华水</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/c8011d074013a6a2805741546e821201.jpg</td><td>吾尊精华水</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/2cca7c1f40075af38095f5c6ab5e359b.jpg</td><td>0</td><td>0</td></tr>";
+code+="<tr><td>6</td><td>覆膜酵母菌BB霜35g</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/0609ce6340cbe75e80b45642ca1eb416.jpg</td><td>上海基金会捐赠。保质期到2019年8月11号</td><td>0</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>7</td><td>一叶子香氛 </td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/f7ba28c1407d98cc80bd24940e20c11e.jpg</td><td>一叶子香氛 </td><td>0</td><td>0</td><td>0</td></tr>";
+code+="<tr><td>7</td><td>覆膜酵母菌爽肤水125ml</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/cb3a2f2c40d40fc5805cc6238ca92270.jpg</td><td>上海基金会捐赠。保质期到2019年7月13号</td><td>0</td><td>0</td><td>0</td></tr>";
 
-code+="<tr><td>8</td><td>献爱心</td><td>无价</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/11/ec869a76404f428980faa8d32a75b463.gif</td><td>献爱心是一种美德无价</td><td>0</td><td>0</td><td>0</td></tr>";
+code+="<tr><td>8</td><td>覆膜酵母菌修护眼霜15ml</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/950ded234017202f806731ae59d067c5.jpg</td><td>上海基金会捐赠。保质期到2019年7月22号</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>9</td><td>积雪草滋养眼霜15ml</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/f327910f402a030d8065d679292c0d79.jpg</td><td>上海基金会捐赠。保质期到2019年7月16号</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/80652dbd401cf7168043284c55028a2d.jpg</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>10</td><td>一叶子香氛</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/f7ba28c1407d98cc80bd24940e20c11e.jpg</td><td>上海基金会捐赠。保质期到2019年12月30号</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>11</td><td>吾尊精华水100ml</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/c8011d074013a6a2805741546e821201.jpg</td><td>上海基金会捐赠。保质期到2021年12月26号</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/2cca7c1f40075af38095f5c6ab5e359b.jpg</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>12</td><td>时克牙膏</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/01d9bb65401ea1d8805be3fbe8e9657f.jpg</td><td>上海基金会捐赠。保质期到2021年8月</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/bf28d72540e5e67c802391da84483c84.jpg</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>13</td><td>祛黑头三部曲</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/082f136d401892cb8076ec43c2fbda36.jpg</td><td>上海基金会捐赠。保质期到2019年4月</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/90e2b203403a217d80e82ac733b3e5ce.png</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>14</td><td>韩束面膜</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/ada4771d4002381180045e26c30b54d3.jpg</td><td>上海基金会捐赠。保质期到</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/13/7d062eb4400bfa7480770d57507d6c30.png</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>15</td><td>韩束蓝藻活漾补水晚安面膜</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/5897749740755c5a80fa6411222f6511.jpg</td><td>上海基金会捐赠。保质期到2021年11月26号</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>16</td><td>献爱心</td><td>无价</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/10/11/ec869a76404f428980faa8d32a75b463.gif</td><td>上海基金会捐赠。保质期到</td><td>0</td><td>0</td><td>0</td></tr>";
 div1.innerHTML=code;} 
