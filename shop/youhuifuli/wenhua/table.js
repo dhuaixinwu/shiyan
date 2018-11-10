cf22fddada7532729b50a86fc5093309cdf8c2e2
haoyouhao <2112343279@qq.com>
2018-11-10 09:30:48 +0800

    hi

---
+++ shop/youhuifuli/wenhua/table.js
@@ -0,0 +1,26 @@
+document.body.onload = table(); function table() { var div1 = document.getElementById("tb1"); var code = ''; code.id="tab"; 
+
+code+="<tr><td>1</td><td>东华彩铅</td><td>9元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/d6b43fa94000ebe58017b2689bdd4893.JPG</td><td>原价是12元哦。有12种颜色呢</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>2</td><td>东华大学钢笔</td><td>89元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/7d142b7a40ab0fce8002fa0622f1da78.JPG</td><td>原价是99元哦。德国品牌哟。</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>3</td><td>东华大学钥匙扣</td><td>9元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/d452636840d54610800d2ae98c74b569.JPG</td><td>原价是10元哦。卖的可快了！</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>4</td><td>东华大学纪念袋</td><td>55元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/58500c9b4068ed45801f50b68d12fd5d.jpg</td><td>原价是59元哦</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/31d0543c4065d6ee806728e4a53cba7e.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/48b1c37d400311888021dd10708fdd5c.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/1d97feda407cbb83800134328be20ba2.jpg</td></tr>";
+
+code+="<tr><td>5</td><td>东华挎包</td><td>35元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/b0dd4f494020940e80e30274905a9464.jpg</td><td>原价是39元哦</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/7571fbc440ab192f8040d0924f2fcc7f.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/10a604b340c78039802b7aecddef49fd.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/62d47396407d58cd806b7b2d7e9f6c97.jpg</td></tr>";
+
+code+="<tr><td>6</td><td>东华帽子</td><td>20元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/ae9ae48640481a6c80f82e540be97ce3.jpg</td><td>原价是25元哦</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/bdf5d0084059909c807fb6a994029e90.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/d81460964010ac1d801462af4bb8667f.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/c916d71c4018fe6c8029f8149484ab60.JPG</td></tr>";
+
+code+="<tr><td>7</td><td>东小阳笔记本</td><td>12元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/306f5f3a40416cbe806f71a95bbf2c94.JPG</td><td>原价是15元哦</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/01d2248440fc480e8095be2073fb7196.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/1789118b404d1990802c73d413a167b4.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/afbc9c9740a8bc1880d83f9e7f7689a5.jpg</td></tr>";
+
+code+="<tr><td>8</td><td>东小阳卡套</td><td>8元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/d9eb9b6940f2a33980ba982e65fbfda4.JPG</td><td>原价是10元哦</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>9</td><td>东小阳信封</td><td>0.2元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/559b2e3a402f7dde8042eff8053f8121.JPG</td><td>原价也是0.2元哦</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>10</td><td>东小阳钥匙扣</td><td>4元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/3dda138a406c7617808aa78bb160776f.JPG</td><td>原价是5元哦</td><td>0</td><td>0</td><td>0</td></tr>";
+
+code+="<tr><td>11</td><td>东华环保购物袋</td><td>10元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/7a9b02604039b65b80fc9c317d7164fb.jpg</td><td>原价是11元哦</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/8dc6b9ea406935e080eae27cb000625d.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/ffbb6e2740217f508023bca1661da2ea.jpg</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/b8861d9940f9def9802ef19312798e2b.jpg</td></tr>";
+
+code+="<tr><td>12</td><td>明信片</td><td>1元</td><td>http://bmob-cdn-20135.b0.upaiyun.com/2018/11/09/82e0c092406c02fe80af072cf4804f96.JPG</td><td>原价也是1元哦。同学们自己拍摄并制作的哟！</td><td>0</td><td>0</td><td>0</td></tr>";
+div1.innerHTML=code;} 
